ae1d1905adf6aeed332f2761544a37f046237828
Alexey Kozlov <alexeykozlov0811@gmail.com>
2019-09-26 17:40:30 +0300

    #Доработана модель текстов: добавить поле автора и уникальности

--- app/models.py
+++ app/models.py
@@ -1,18 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
+
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    teacher = models.BooleanField(default=False)
+
+    #def __init__(self, ):
+
+
 class Text(models.Model):
-    source = models.CharField(max_length=255, verbose_name = "Джерело")
-    content = models.TextField(verbose_name = "Текст")
+    author = models.CharField(max_length=255, verbose_name="Автор", default="Unknown")
+    source = models.CharField(max_length=255, verbose_name="Джерело")
+    content = models.TextField(verbose_name="Текст")
+    uniqueness = models.FloatField(default=0)
 
     def __str__(self):
-        return 'Джерело - {0}'.format(self.source)
+        return 'Джерело - {0}'.format(self.author)
 
-    def create(self, source, content):
-        text = Text()
-        text.source = source
-        text.content = content
-        text.save()
-        return text
+    # def __init__(self, source, content, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.source = source
+    #     self.content = content
+    #     # self.save()
 
+    # def set_uniqueness(self, uniqueness):
+    #     self.uniqueness = uniqueness
+    #     self.save()
--- app/views.py
+++ app/views.py
@@ -57,6 +57,7 @@ def get(request):
     """Renders the about page."""
     assert isinstance(request, HttpRequest)
     all_text = Text.objects.all()
+    # Text.objects.all().delete()
     return render(
         request,
         'app/text_list.html',
@@ -67,46 +68,46 @@ def get(request):
 
 
 def create(request):
-    """Renders the about page."""
     assert isinstance(request, HttpRequest)
-    if request.method == "POST":
-        text = Text()
-        text.source = request.POST.get("Source")
-        text.content = request.POST.get("Content")
-        text.save()
+    text = Text()
+    if request.user.is_authenticated:
+        text.author = request.user.username
+    text.source = request.POST.get("Source")
+    text.content = request.POST.get("Content")
+    text.save()
     return HttpResponseRedirect("/")
 
 
 def check_uniq(request):
-    if request.method == "POST":
-        text = Text()
-        text.source = request.POST.get("Source")
-        text.content = request.POST.get("Content")
-        sources = []
-        percentage = []
-        all_text = Text.objects.all()
-        for text2 in all_text:
-            sources.append(text2.source)
-            percentage.append(compaire_texts(text.content, text2.content))
-            # uniq.update({text2.source : percentage})
-
-        urls = list(set(find_text_urls(text.content)))
-        for i in all_text:
-            try:
-                urls.remove(i.source)
-            except ValueError:
-                continue
-
-        sources.extend(urls)
-
-        for url in urls:
-            text2 = Text()
-            text2.source = url
-            text2.content = get_content(url)
-            percentage.append(compaire_texts(text.content, text2.content))
-            # uniq.update({text2.source : percentage})
-            text2.save()
-
-        if 100.0 not in percentage:
-            text.save()
-        return render(request, 'app/uniq.html', {'uniq': sources, 'percentage': percentage})
+    # text = Text()
+    #     # text.source = request.POST.get("Source")
+    #     # text.content = request.POST.get("Content")
+    sources = 1
+    percentage = 100
+    # all_text = Text.objects.all()
+    # for text2 in all_text:
+    #     sources.append(text2.source)
+    #     percentage.append(compaire_texts(text.content, text2.content))
+    #     # uniq.update({text2.source : percentage})
+    #
+    # urls = list(set(find_text_urls(text.content)))
+    # for i in all_text:
+    #     try:
+    #         urls.remove(i.source)
+    #     except ValueError:
+    #         continue
+    #
+    # sources.extend(urls)
+    #
+    # for url in urls:
+    #     text2 = Text()
+    #     text2.source = url
+    #     text2.content = get_content(url)
+    #     percentage.append(compaire_texts(text.content, text2.content))
+    #     # uniq.update({text2.source : percentage})
+    #     text2.save()
+    #
+    # if 100.0 not in percentage:
+    #     text.save()
+    # return render(request, 'app/uniq.html', {'uniq': sources, 'percentage': percentage})
+    return HttpResponseRedirect("/")
