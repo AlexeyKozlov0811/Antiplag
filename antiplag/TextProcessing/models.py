f6b1a8f2c9123b13f84e9939327ee0f2606e3c14
Alexey Kozlov <alexeykozlov0811@gmail.com>
2019-11-27 20:04:28 +0200

    Добавлена функция удаления дубликатов из списка заимствованных шинглов Добавлено новое поле БД, которое содержит канонизированные шинглы текста

--- antiplag/TextProcessing/models.py
+++ antiplag/TextProcessing/models.py
@@ -7,6 +7,7 @@ class Text(models.Model):
     author = models.CharField(max_length=255, verbose_name="Автор", default="Unknown")
     source = models.CharField(max_length=255, verbose_name="Джерело", default="Author")
     content = models.TextField(verbose_name="Текст")
+    shingled_content = models.TextField(null=True ,verbose_name="шинглований канонічний текст")
     uniqueness = models.FloatField(default=-1, verbose_name="Унікальність")
     upload_date = models.DateField(default=datetime.today(), verbose_name="Дата завантаження")
 
--- antiplag/TextProcessing/tasks.py
+++ antiplag/TextProcessing/tasks.py
@@ -1,7 +1,10 @@
-from celery import shared_task
-from .DataProcessing import Parser, Shingling
-from .models import Text
 
+
+from celery import shared_task
+from .DataProcessing.Parser import *
+from .DataProcessing.Shingling import *
+from .models import Text
+import json
 
 @shared_task
 def check_uniqueness(user_text_id):
@@ -13,15 +16,16 @@ def check_uniqueness(user_text_id):
 @shared_task
 def check_web(user_text_id):
     user_text = Text.objects.get(id=user_text_id)
-    SetOfUrls = Parser.find_text_urls(user_text.content)
+    SetOfUrls = find_text_urls(user_text.content)
     if SetOfUrls:
         for url in SetOfUrls:
             if not Text.objects.filter(source=url).exists():
                 web_text = Text()
                 web_text.source = url
-                web_text.content = Parser.get_content(url)
+                web_text.content = get_content(url)
                 web_text.uniqueness = -1.0
-                if web_text.content != "0":
+                if web_text.content != 0:
+                    web_text.shingled_content = json.dumps(shingle_generation(canonize(web_text.content), 0))
                     web_text.save()
                 else:
                     del web_text
@@ -32,14 +36,26 @@ def check_web(user_text_id):
 
 @shared_task
 def check_database(user_text_id):
+    sources_id = []
     similar_parts = []
     user_text = Text.objects.get(id=user_text_id)
-    shingle_dict = Shingling.shingle_generation(Shingling.canonize(user_text.content), 1)
-    shingled_user_text = Shingling.shingle_generation(Shingling.canonize(user_text.content), 0)
+    user_shingled_content = shingle_generation(canonize(user_text.content), 0)
+    user_text.shingled_content = json.dumps(user_shingled_content)
+
     for data_base_text in Text.objects.exclude(id=user_text_id).exclude(uniqueness=0.0):
-        shingled_db_text = Shingling.shingle_generation(Shingling.canonize(data_base_text.content), 0)
-        similar_parts += (Shingling.comparation(shingled_user_text, shingled_db_text))
-    user_text.uniqueness = Shingling.similarity_percentage_calculation(shingled_user_text, similar_parts)
-    print(user_text.uniqueness)
+        similar_part = comparation(user_shingled_content,
+                                   json.loads(data_base_text.shingled_content))
+        if similar_part:
+            sources_id.append(data_base_text.id)
+            similar_part = duplicate_clear(similar_parts, similar_part)
+            similar_parts += similar_part
+            # similar_parts.append(".")
+
+    # similar_parts.remove(".")
+    user_text.uniqueness = similarity_percentage_calculation(user_shingled_content, similar_parts)
+    if user_text.uniqueness < 0:
+        user_text.uniqueness = 0.0
+    print(sources_id)
     user_text.save()
     return 0
+
