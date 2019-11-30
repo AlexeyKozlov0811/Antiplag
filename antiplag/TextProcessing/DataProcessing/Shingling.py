8e37dcf522d821ae79e94243910a9c83c4053f00
Raido-Isaz <aporena22@gmail.com>
2019-12-01 01:22:45 +0200

    Добавлена функция поиска и удаления дубликатов, исправлен подсчет уникальности

--- antiplag/TextProcessing/DataProcessing/Shingling.py
+++ antiplag/TextProcessing/DataProcessing/Shingling.py
@@ -1,68 +1,56 @@
-# coding=utf-8
-# from hash_encryption import encryption
+# -*- coding: utf-8 -*-
+
 import binascii
 from pymorphy2 import MorphAnalyzer
+from StopSymbols import *
 
 morph = MorphAnalyzer(lang='uk')
 
 
 def canonize(source):
-    stop_symbols = '[].,!?:;-\'\"—\n\r()«»'
-    stop_words = (u'і', u'й', u'так',  # сполучники
-                  u'а', u'але', u'та',
-                  u'бо', u'як', u'що',
-                  u'на', u'ніби', u'неначе',
-                  u'якже', u'якби', u'якщо',
-                  u'проте', u'зате', u'як',
-                  u'де', u'тому що', u'доки',
-                  u'мов', u'наче', u'немов',
-                  u'цебто', u'оскільки', u'однак',
-                  u'тільки', u'хоч', u'хай',
-                  u'аби', u'відтоді', u'тобто',
-                  u'нібито', u'щоб', u'аби',
-                  u'без', u'біля', u'до',  # прийменники
-                  u'зза', u'ізза', u'проти',
-                  u'серед', u'назустріч', u'напереріз',
-                  u'наперекір', u'усупереч', u'навздогін',
-                  u'над', u'під', u'перед',
-                  u'поза', u'на', u'об',
-                  u'о', u'по', u'з',
-                  u'із', u'зі', u'між',
-                  u'за', u'у', u'в',
-                  u'відповідно до', u'для', u'до',
-                  u'через', u'тощо',
-                  u'я', u'ми', u'ти',  # займенники
-                  u'ви', u'він', u'вона',
-                  u'воно', u'вони', u'себе',
-                  u'мій', u'твій', u'наш',
-                  u'свій', u'ваш', u'його',
-                  u'її', u'їх', u'їхній',
-                  u'той', u'цей', u'такий',
-                  u'стільки')
-
     text_cleaning = [x for x in [y.strip(stop_symbols) for y in source.lower().split()] if x and (x not in stop_words)]
     cleaned_text = []
+    # print(text_cleaning)
     for i in range(len(text_cleaning)):
         cleaned_text.append(morph.parse(text_cleaning[i])[0].normal_form)
+    # print(cleaned_text)
     return cleaned_text
-    # return ([x for x in [y.strip(stop_symbols) for y in source.lower().split()] if x and (x not in stop_words)])
 
 
-# generates shingles, if key = 1, that means that we make shingles out of canonized text, if key = 0, ...out of hashes
-def shingle_generation(source, key):
+# generates shingles
+def shingle_generation(source):
     shingle_len = 4  # длина шингла
     out = []
     words = []
     words_crc = []
     for i in range(len(source) - (shingle_len + 1)):
-        if key == 1:
-            words.append(' '.join([x for x in source[i:i + shingle_len]]))
-            words_crc.append(binascii.crc32(' '.join([x for x in source[i:i + shingle_len]]).encode('utf-8')))
         out.append(binascii.crc32(' '.join([x for x in source[i:i + shingle_len]]).encode('utf-8')))
     words_dict = dict(zip(words_crc, words))  # zip -  creates dictionary where keys are elements of words_crc
-    if key == 1:
-        return words_dict
     return out
+
+
+def text_splitting(text):
+    hashes = shingle_generation(canonize(text))
+    separated_text = text.split()
+    separated_list = []
+    separated_str = ""
+    shingle_len = 4
+    i = 0
+    while int(i) < len(separated_text):
+        if separated_text[i] not in stop_words:
+            shingle_len -= 1
+            if shingle_len == 2:
+                start_index = i
+        separated_str += separated_text[i] + " "
+        if shingle_len == 0:
+            separated_list.append(separated_str)
+            shingle_len = 4
+            separated_str = ""
+            i = start_index
+        else:
+            i += 1
+    words_dict = dict(zip(hashes, separated_list))
+    return words_dict
 
 
 # compares texts and returns list of similar hashed shingles
@@ -73,19 +61,19 @@ def comparation(source1, source2):
         if source1[i] in source2:
             # same = same + 1
             similar_phrases.append(source1[i])
-    print(similar_phrases)
+    # print(similar_phrases)
     return similar_phrases
 
 
-def DuplicateSearch(similar_phrases_1, similar_phrases_2):
-    without_duplicates = similar_phrases_1
-    if similar_phrases_1 == similar_phrases_2:
-        return similar_phrases_1
-    else:
-        for i in range(len(similar_phrases_2)):
-            if similar_phrases_2[i] not in without_duplicates:
-                without_duplicates.append(similar_phrases_2[i])
-    return without_duplicates
+# def DuplicateSearch(similar_phrases_1, similar_phrases_2):
+#     without_duplicates = similar_phrases_1
+#     if similar_phrases_1 == similar_phrases_2:
+#         return similar_phrases_1
+#     else:
+#         for i in range(len(similar_phrases_2)):
+#             if similar_phrases_2[i] not in without_duplicates:
+#                 without_duplicates.append(similar_phrases_2[i])
+#     return without_duplicates
 
 
 # puts together all the words that
@@ -109,6 +97,19 @@ def similar_areas_definition(text1_dictionary, compared_texts):
 
     defined_area = defined_area + areas[(len(areas) - 1)]
     return defined_area
+
+
+def duplicate_clear(old_similar_phrases, new_similar_phrases):
+    duplicates_indexes = []
+    for word_in_old in old_similar_phrases:
+        for word_in_new in new_similar_phrases:
+            if word_in_new == word_in_old:
+                duplicates_indexes.append(new_similar_phrases.index(word_in_new))
+    duplicates_indexes = list(set(duplicates_indexes))
+    duplicates_indexes.sort(reverse=True)
+    for duplicate in duplicates_indexes:
+        del new_similar_phrases[duplicate]
+    return new_similar_phrases
 
 
 # counts the percentage of two texts, will be changed later
@@ -146,15 +147,33 @@ if __name__ == "__main__":
             u'на межі Полісся і лісостепу по обидва береги Дніпра в його середній течії. Площа міста 836 км. Довжина ' \
             u'вздовж берега — понад 20 км. '
 
-    shingle_dict = shingle_generation(canonize(text1), 1)
+    shingle_dict = text_splitting(text1)
 
-    shingled_canonized_text1 = shingle_generation(canonize(text1), 0)
-    shingled_canonized_text2 = shingle_generation(canonize(text2), 0)
-    shingled_canonized_text3 = shingle_generation(canonize(text3), 0)
+    print(shingle_dict)
 
-    similar = DuplicateSearch(comparation(shingled_canonized_text1, shingled_canonized_text2), comparation(shingled_canonized_text1, shingled_canonized_text3))
+    shingled_canonized_text1 = shingle_generation(canonize(text1))
+    shingled_canonized_text2 = shingle_generation(canonize(text2))
+    shingled_canonized_text3 = shingle_generation(canonize(text3))
 
-    print(similar_areas_definition(shingle_dict, similar))
+    # similar = DuplicateSearch(comparation(shingled_canonized_text1, shingled_canonized_text2), comparation(shingled_canonized_text1, shingled_canonized_text3))
 
-    print(similarity_percentage_calculation(shingled_canonized_text1, similar))
 
+
+    similar_1 = comparation(shingled_canonized_text1, shingled_canonized_text2)
+    similar_2 = comparation(shingled_canonized_text1, shingled_canonized_text3)
+
+    # print(similar_1)
+    #
+    # print(similar_2)
+    #
+    # print(similar_1 + similar_2)
+
+    similar_2 = duplicate_clear(similar_1, similar_2)
+
+    similar_1 += similar_2
+
+    print(similar_1)
+
+    print(similar_areas_definition(shingle_dict, similar_1))
+
+    print(similarity_percentage_calculation(shingled_canonized_text1, similar_1))
